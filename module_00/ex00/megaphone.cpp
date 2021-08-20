3df333b5cd9e6ce3e7cd50f4abec8088179de7fc
Diogo Cavaleiro <dcavalei@mx>
2021-08-20 20:10:30 +0100

    Module_00 - ex00 done

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1) // no arguments case
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; argv[i][j]; j++) {
+			std::cout << (char)std::toupper(argv[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
